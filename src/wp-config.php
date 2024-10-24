66eaf5b85c2bff0bfd52cda4e9c23dff05b72bc4
JasonHoups <6015140@mborijnland.nl>
2024-10-24 13:46:17 +0200

    docker container geadd

--- src/wp-config.php
+++ src/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'techsphere' );
+// define( 'DB_NAME', 'techsphere' );
 
-/** Database username */
-define( 'DB_USER', 'Jason' );
+// /** Database username */
+// define( 'DB_USER', 'Jason' );
 
-/** Database password */
-define( 'DB_PASSWORD', '1234' );
+// /** Database password */
+// define( 'DB_PASSWORD', '1234' );
 
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+// /** Database hostname */
+// define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
