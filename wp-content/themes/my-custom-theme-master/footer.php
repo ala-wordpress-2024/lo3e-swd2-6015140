2fe51b1d7b4698ad62bf94a8dcfe09f54230df1c
JasonHoups <6015140@mborijnland.nl>
2024-10-24 11:50:00 +0200

    laatste commit

--- wp-content/themes/my-custom-theme-master/footer.php
+++ wp-content/themes/my-custom-theme-master/footer.php
@@ -1,11 +1,30 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <?php
+
 /**
  * The footer template part.
  *
  * @copyright  Copyright (c) 2020, Danny Cooper
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
+wp_footer(); ?>
+<footer class="footers">
+<div class="footerGegevens">
+	<p>
+		Contactgegevens
+	</p>
+	<p>
+		Betaplein 18. 2204vh. 06123456789.
+	</p>
+	<a href="https://www.instagram.com/" class="fa fa-instagram"></a>
+	<a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+	<a href="https://telegram.org/" class="fa fa-flickr"></a>
+	<a href="https://x.com/?lang=en" class="fa fa-twitter"></a>
+</div>
 
-		wp_footer(); ?>
-	</body>
+
+</footer>
+</body>
+
 </html>
--- wp-content/themes/my-custom-theme-master/index.php
+++ wp-content/themes/my-custom-theme-master/index.php
@@ -1,3 +1,5 @@
+
+
 <?php
 /**
  * The main index template.
@@ -5,6 +7,8 @@
  * @copyright  Copyright (c) 2020, Danny Cooper
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
+
+ 
 
 get_header(); ?>
   <div class="site-content">
